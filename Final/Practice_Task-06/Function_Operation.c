a8d4ae9f4526cf46456dd9f34cca1caaec2b1ac0
Showmik Sharma <showmiksharma2180@gmail.com>
2026-08-07 21:02:14 +0600

    Add files via upload

--- Final/Practice_Task-06/Function_Operation.c
+++ Final/Practice_Task-06/Function_Operation.c
@@ -4,25 +4,21 @@ void add(int a, int b)
     int sum = a + b;
     printf("%d\n",sum);
 }
-
 void sub(int a, int b)
 {
     int sub = a - b;
     printf("%d\n",sub);
 }
-
 void mul(int a, int b)
 {
     int mul = a * b;
     printf("%d\n",mul);
 }
-
 void div(float a, float b)
 {
     float div = a / b;
     printf("%.2f",div);
 }
-
 int main()
 {
     int n,m;
@@ -31,6 +27,5 @@ int main()
     sub(n,m);
     mul(n,m);
     div(n,m);
-
     return 0;
 }
